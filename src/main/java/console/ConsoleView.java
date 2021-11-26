c92241b5373d8ef760ae037e71eab2dec651cd1f
Konst <kreogen@email.ua>
2021-11-26 08:42:09 +0200

    26.11.21 Окно заготовка для выбора периода в xls

--- src/main/java/console/ConsoleView.java
+++ src/main/java/console/ConsoleView.java
@@ -15,16 +15,20 @@ package console;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Optional;
 
 
 /**
@@ -64,6 +68,11 @@ public class ConsoleView extends BorderPane {
         throw new RuntimeException(e1);
       }
     }));
+    menu.getItems().add(createItem("Select xls", e -> {
+      System.out.println("Select xls");
+      System.out.println(showDialogXls());
+    }));
+
     menu.getItems().add(createItem("Exit", e -> {
       System.out.println("Exit");
       Platform.exit();
@@ -87,5 +96,51 @@ public class ConsoleView extends BorderPane {
   public InputStream getIn() {
     return in;
   }
+
+  String showDialogXls(){
+    Dialog<String> dialog = new Dialog<>();
+    dialog.setTitle("Экспорт данных");
+    dialog.setHeaderText("Это окно для выбора периода данных для экспорта в xls \n" +
+            "нажмите OK (или нажмите закрыть 'x').");
+    dialog.setResizable(true);
+
+    Label label1 = new Label("Дата с: ");
+    Label label2 = new Label("Дата по: ");
+    DatePicker from = new DatePicker();
+    DatePicker to = new DatePicker();
+    //TextField text1 = new TextField();
+    //TextField text2 = new TextField();
+
+    GridPane grid = new GridPane();
+    grid.add(label1, 1, 1);
+    grid.add(from, 2, 1);
+    grid.add(label2, 1, 2);
+    grid.add(to, 2, 2);
+    dialog.getDialogPane().setContent(grid);
+
+    ButtonType buttonTypeOk = new ButtonType("OK", ButtonBar.ButtonData.OK_DONE);
+    dialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+
+    dialog.setResultConverter(new Callback<ButtonType, String>() {
+      @Override
+      public String call(ButtonType b) {
+
+        if (b == buttonTypeOk) {
+          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+          LocalDate date = from.getValue();
+
+          String result = String.format("from: %s to: %s",from.getValue().format(formatter),to.getValue().format(formatter));
+          return result;//new PhoneBook(text1.getText(), text2.getText());
+        }
+
+        return null;
+      }
+    });
+
+    Optional<String> result = dialog.showAndWait();
+    return result.get();
+  }
+
+
 
 }
